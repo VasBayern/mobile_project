a801688556fef599d0b9da6f156d107f68423ca7
VasBayern <vastb98@gmail.com>
2021-04-11 15:49:02 +0700

    install swagger

--- mobile_shop_api/app/Http/Controllers/Controller.php
+++ mobile_shop_api/app/Http/Controllers/Controller.php
@@ -9,5 +9,34 @@ use Illuminate\Routing\Controller as BaseController;
 
 class Controller extends BaseController
 {
+    /**
+     * @OA\Info(
+     *      version="1.0.0",
+     *      title="Integration Swagger in Laravel with Sanctum Auth Documentation",
+     *      description="Implementation of Swagger with in Laravel",
+     *      @OA\Contact(
+     *          email="admin@admin.com"
+     *      ),
+     *      @OA\License(
+     *          name="Apache 2.0",
+     *          url="http://www.apache.org/licenses/LICENSE-2.0.html"
+     *      )
+     * )
+     *
+     * @OA\Server(
+     *      url="http://127.0.0.1:8000/api",
+     *      description="API Server"
+     * )
+     * 
+     * @OA\SecurityScheme(
+     *      securityScheme="bearerAuth",
+     *      in="header",
+     *      type="http",
+     *      scheme="bearer",
+     *      bearerFormat="JWT",
+     *      name="Authorization",
+     * )
+     * 
+     */
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
 }
